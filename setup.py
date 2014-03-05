a7b84b8333c43b4005cbb9b486239f409bf450b6
Marc Brinkmann <git@marcbrinkmann.de>
2014-03-05 18:22:31 +0100

    Slight cleanup in setup.py.

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@ import sys
 
 from setuptools import setup, find_packages
 
-long_desc = open('README.rst').read()
 
 setup(name="flatland0",
       version='0.0.1.dev1',
-      packages=find_packages(exclude=['tests.*', 'tests']),
       author='Jason Kirtland',
       author_email='jek@discorporate.us',
       description='HTML form management and validation',
       keywords='schema validation data web form forms roundtrip',
-      long_description=long_desc,
+      long_description=open('README.rst').read(),
       license='MIT License',
       url='https://github.com/mbr/flatland0',
+      packages=find_packages(exclude=['tests']),
       include_package_data=True,
       zip_safe=True,
       install_requires=[
           'blinker', 'six',
           ],
-      **extra_setup)
+      )
