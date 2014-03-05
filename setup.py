db8da8f9e3f3af085a67175d68f548e850302521
Marc Brinkmann <git@marcbrinkmann.de>
2014-03-05 18:19:45 +0100

    Removed some cruft from setup.py.

--- setup.py
+++ setup.py
@@ -1,19 +1,7 @@
 import os
 import sys
 
-try:
-    from setuptools import setup, find_packages
-    extra_setup = dict(
-        include_package_data=True,
-        zip_safe=True,
-        )
-except ImportError:
-    from distutils.core import setup
-    extra_setup = {}
-    def find_packages(exclude=()):
-        return [w[0].replace('/', '.')
-                for w in os.walk('flatland')
-                if '__init__.py' in w[2]]
+from setuptools import setup, find_packages
 
 import flatland
 version = flatland.__version__
@@ -28,19 +16,9 @@ setup(name="flatland",
       keywords='schema validation data web form forms roundtrip',
       long_description=long_desc,
       license='MIT License',
-      url='http://discorporate.us/jek/projects/flatland/',
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: MIT License',
-                   'Natural Language :: English',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Programming Language :: Python :: 2.5',
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
-                   'Topic :: Internet :: WWW/HTTP :: WSGI',
-                   'Topic :: Software Development :: Libraries'],
+      url='https://github.com/mbr/flatland0',
+      include_package_data=True,
+      zip_safe=True,
       install_requires=[
           'blinker', 'six',
           ],
